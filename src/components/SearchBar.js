eaa470d39f39d470c35b9534f29e5392d6ffeeae
tomkf <thomaskeithferris@gmail.com>
2020-02-26 15:42:52 -0800

    search bar with hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import { StyleSheet, View, TextInput } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons'
 
-const SearchBar = () => {
-  return (
-    <View>
-      <TextInput placeholder="Search" />
-    </View>
-  );
-}
+const SearchBar = ({ term, onTermChange }) => {
+    return (
+      <View style={styles.background}>
+        <EvilIcons style={styles.icon} name="search" />
+        <TextInput
+          value={term}
+          style={styles.input}
+          placeholder="Search"
+          onChangeText={newTerm => onTermChange(newTerm)} />
+      </View>
+    );
+  }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    height: 50,
+    borderRadius: 6,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  input: {
+    flex: 1
+  },
+  icon: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
+  }
+});
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('')
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar term={term} onTermChange={(newTerm) => setTerm(newTerm)} />
     </View>
   );
 }
