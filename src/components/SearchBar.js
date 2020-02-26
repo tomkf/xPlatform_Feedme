66967e7fc269be9e3f5792b96ba18bf685279a6b
tomkf <thomaskeithferris@gmail.com>
2020-02-26 15:40:06 -0800

    search screen components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+
+const SearchBar = () => {
+  return (
+    <View>
+      <TextInput placeholder="Search" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchBar;
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import SearchBar from '../components/SearchBar';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <SearchBar />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
