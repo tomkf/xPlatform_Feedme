851774d44d7641ecfdcd6e144c03863e639925a5
Thomas Ferris <A01211467@ad.bcit.ca>
2020-02-28 15:59:51 -0800

    navigate to detail view

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,1 +1,22 @@
 
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+
+const DetailScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Detail!</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  }
+});
+
+export default DetailScreen;
